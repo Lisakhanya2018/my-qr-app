86150fef62c56a968cc431ed6cc5abab31611f5c
Nomthandazo <nomthandazomdlovulu@gmail.com>
2026-01-21 19:19:52 +0200

    updates3

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import qrImage from "./assets/qrcode.jpg";
 function App() {
   return (
     <div className="table-bg">
+      <h1 className="menu-title">MENU</h1>
+      <h2 className="sub-title">Scan the QR code to view the menu</h2>
       <div className="qr-container">
       <img src={qrImage} alt="QR Code" className="qr-image" />
       </div>
