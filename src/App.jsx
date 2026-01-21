508f5294aa1eb413f62fd747f41608de9d820e33
Nomthandazo <nomthandazomdlovulu@gmail.com>
2026-01-21 17:52:47 +0200

    new changes2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import qrImage from "./assets/qrcode.jpg";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="table-bg">
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        
-        </a>
+      <div className="qr-container">
+      <img src={qrImage} alt="QR Code" className="qr-image" />
       </div>
-
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
